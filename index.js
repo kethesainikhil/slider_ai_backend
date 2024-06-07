3ef024c9fc72195bf666d5367e8400126f4b6c6f
kethe sai nikhil <nikhilkethe007@gmail.com>
2024-06-08 00:54:59 +0530

    some

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' })); 
-// const openai = new OpenAI( {apiKey: "sk-proj-uevg3bNsojCqQyeqkhhKT3BlbkFJA0vRLxuPIFtwWitRJ7Sd"} );
 
 app.use(express.json());
 
